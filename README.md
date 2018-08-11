d338aef6fe1975b66847a69c878158b2e83aed74
honu <honu@Honu-MacBook-Air.local>
2018-08-11 18:38:45 +0900

    create super user, images app. deleted templated

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Honugram
 
 Study frontend and backend 
+
+## anaconda env
+pip install -r requirement/local.txt
+
+because conda is imemdiately virtual env.
+
+## admin password
+test 1234
--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ DEBUG = env.bool('DJANGO_DEBUG', False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = 'Asia'
+TIME_ZONE = 'Asia/Seoul'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -63,7 +63,7 @@ DJANGO_APPS = [
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
-    'crispy_forms',
+    # 'crispy_forms', 이거 필요 없다는데 뭔지 찾아보자
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -72,6 +72,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'honugram.users.apps.UsersAppConfig',
     # Your stuff: custom apps go here
+    'honugram.images.apps.ImagesConfig'
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
