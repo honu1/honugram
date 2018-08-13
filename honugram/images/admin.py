3caa9e53ee123373eba9788d62d4f17e04f5dff0
honu <honu@Honu-MacBook-Air.local>
2018-08-13 17:04:46 +0900

    modified model. images, comment, like. and add orm

--- honugram/images/admin.py
+++ honugram/images/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+
+    # this admin model can modified image file link
+    list_display_links = (
+        'location',
+    )
+
+    search_fileds = (
+        'location',
+        'caption',
+    )
+
+    list_filter = (
+        'location',
+        'caption',
+    )
+
+    list_display = (
+        'file',
+        'location',
+        'caption',
+        'creator',
+        'created_at',
+        'updated_at',
+    )
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = (
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'message',
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
--- honugram/images/models.py
+++ honugram/images/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from honugram.users import models as user_model
 
-# Create your models here.
+# @python_2_unicode_competible - this decoration is only python2.
+# my django env is only pyhthon3. so i don't need it.
+class TimeStampModel(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Image(TimeStampModel):
+
+    """ Image Model """
+    file = models.ImageField()
+    location = models.CharField(max_length = 140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+            return "{} - {}".format(self.location, self.caption)
+
+class Comment(TimeStampModel):
+
+    """ Comment Model """
+    message = models.TextField()
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+            return self.message
+
+class Like(TimeStampModel):
+    
+    """ Like Model """
+    creator = models.ForeignKey(user_model.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+            return "User: {} - Image Caption: {}".format(self.creator.username, self.image.caption)
