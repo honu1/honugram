85f200602085c92ddebdb9edc0f504142e636ea7
정현우 <hwjung@hancom.com>
2018-08-14 17:40:52 +0900

    create list all image view

--- honugram/images/views.py
+++ honugram/images/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.view import APIView
+from rest_framework.response import Response
+from . import models, serializers
 
-# Create your views here.
+# viewing all image in database.
+# this practic test. service dev is not adjust to it.
+
+class ListAllImages(APIView):
+
+    def get(self, request, format=None):
+
+        all_images =  models.Image.objects.all()
+
+        serializer = serializers.ImageSerializer(all_images, many=True)
+
+        return Response(data=serializer.data)
