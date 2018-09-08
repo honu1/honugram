2457ba49ed058e7932737654a50d7cac2eca30c4
honu <honu@Honu-MacBook-Air.local>
2018-09-08 17:54:00 +0900

    update password

--- honugram/users/urls.py
+++ honugram/users/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("<int:user_id>/unfollow/", view=views.UnFollowUser.as_view(), name="unfollow_user"),
     path("<username>/followers/", view=views.UserFollowers.as_view(), name="user_followers"),
     path("<username>/following/", view=views.UserFollowing.as_view(), name="user_following"),
+    path("<username>/password/", view=views.ChangePassword.as_view(), name="change_password"),
     path("search/", view=views.Search.as_view(), name="search"),
     path("<username>", view=views.UserProfile.as_view(), name="user_profile"),
 ]
--- honugram/users/views.py
+++ honugram/users/views.py
@@ -136,4 +136,29 @@ class Search(APIView):
 
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
-        
+
+class ChangePassword(APIView):
+
+    def put(self, request, username, format=None):
+
+        user = request.user
+
+        if username == user.username:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        current_password = request.data.get('current_password', None)
+
+        if current_password is not None:
+            password_match = user.check_password(current_password)
+            
+            if password_match:
+
+                new_password = request.data.get('new_password', None)
+
+                if new_password is not None:
+                    user.set_password(new_password)
+                    user.save()
+
+                    return Response(status=status.HTTP_200_OK)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
