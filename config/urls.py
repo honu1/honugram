f3f4e58ea337d3aadc15f8e0536171501c9294bb
honu <honu@nate.com>
2018-09-22 18:25:07 +0900

    Creating React App View Adding Corsheader in Django

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token
+from honugram import views
 
 urlpatterns = [
 
@@ -27,6 +28,8 @@ urlpatterns = [
     ),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    path("", views.ReactAppView.as_view()),
+
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
