e5defc1af0233a03e413c5b0868c5b236032212a
honu <honu@nate.com>
2018-09-23 22:00:01 +0900

    adding redux history

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,22 +1,29 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 import users from 'redux/modules/users';
-
-import { logger } from 'redux-logger'
 
 const env = process.env.NODE_ENV;
 
-const middlewares = [thunk];
+const history = createHistory();
+
+// it need to conecting routerRuducer and history objcect 
+const middlewares = [thunk, routerMiddleware(history)];
 
 if(env === 'development') {
-    const {logger} =  require('redux-logger');
+    const { logger } =  require('redux-logger');
     middlewares.push(logger);
 }
 
 const reducer =  combineReducers({
     users,
+    router: routerReducer
 });
 
 let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+
+//it need to making router.
+export { history };
 
 export default store();
