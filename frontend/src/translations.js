604d394986984eae7501b4adbdf1fc03445fb286
honu <honu@nate.com>
2018-09-29 18:06:33 +0900

    addded importing, extracting translations.

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -1,33 +1,31 @@
 export const translations = {
-  ko: {
-    //Auth
-    "Don't have an account?":"계정이 없으신가요?",
-    "Have an account?": "계정이 있으신가요?",
-    "Get the app": "앱을 다운로드하세요",
-    //Login Form
-    "Username":"사용자 이름",
-    "Password":"비밀번호",
-    "Log in":"로그인",
-    "Log in with Facebook":"Facebook으로 로그인",
-    "Forgot password?":"비밀번호를 잊으셨나요?",
-    "Loggin In": "로그인",
-    //Sing Up Form
-    "Sign up to see photos and videos from your friends.":"친구들의 사진과 동영상을 보려면 가입하세요.",
-    "or": "또는",
-    "Email": "이메일 주소",
-    "Full Name": "성명",
-    "By signing up, you agree to our":"가입하면 Instagram의 약관, 데이터 정",
-    "Terms & Privacy Policy": "약관, 데이터 정책 및 쿠키 정책에 동의하게 됩니다.",    
-    //footer
-    "About Us": "INSTAGRAM 정보",
-    "Support": "지원",
-    "Blog": "블로그",
-    "Press": "홍보 센터",
-    "API": "API",
-    "Jobs": "채용 정보",
-    "Privacy": "개인정보처리방침",
-    "Terms": "약관",
-    "Directory": "디렉터리",
-    "Language": "언어",
-  }
-};
+  'ko': {
+    'Don\'t have an account?': '계정이 없으신가요?',
+    'Sign up': '로그인',
+    'Have an account?': '계정이 있으신가요?',
+    'Log in': '로그인',
+    'Get the app': '앱을 다운로드하세요',
+    'Username': '사용자 이름',
+    'Password': '비밀번호',
+    'or': '또는',
+    'Log in with Facebook': 'Facebook으로 로그인',
+    'Forgot password?': '비밀번호를 잊으셨나요?',
+    'Sign up to see photos and videos from your friends.': '친구들의 사진과 동영상을 보려면 가입하세요.',
+    'Email': '이메일 주소',
+    'Full Name': '성명',
+    'By signing up, you agree to our': '가입하면 Instagram의 약관, 데이터 정',
+    'Terms & Privacy Policy': '책 및 쿠키 정책에 동의하게 됩니다',
+    'About Us': 'INSTAGRAM 정보',
+    'Support': '지원',
+    'Blog': '블로그',
+    'Press': '홍보 센터',
+    'API': 'API',
+    'Jobs': '채용 정보',
+    'Privacy': '개인정보처리방침',
+    'Terms': '약관',
+    'Directory': '디렉터리',
+    'Language': '언어',
+  },
+  'options': {
+  },
+}
